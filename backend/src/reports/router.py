ffd4cbbd9cc024196ce872a256b617d88c0e9b6c
risbern21 <risbernfernandes21@gmail.com>
2026-04-21 10:18:02 +0530

    defined auth and report routes

--- backend/src/reports/router.py
+++ backend/src/reports/router.py
@@ -3,4 +3,26 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/reports", tags=["Reports"])
 
 
-@router
+@router.post("/add")
+def add_report():
+    pass
+
+
+@router.get("/get/:id")
+def get_report():
+    pass
+
+
+@router.get("/get/:user_id")
+def get_all_my_reports():
+    pass
+
+
+@router.put("/change")
+def update_report():
+    pass
+
+
+@router.delete("/remove")
+def delete_report():
+    pass
